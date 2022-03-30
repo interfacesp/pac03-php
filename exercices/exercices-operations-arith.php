24876c8cc8805801a91e0e6d6f2c047ab7ebc9c9
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-03-30 23:27:18 +0200

    exos arith

--- exercices/exercices-operations-arith.php
+++ exercices/exercices-operations-arith.php
@@ -3,7 +3,62 @@
 /**
  * Exercice 1 
  * 
+ * Sachant qu'à Interface3, il y a des formations différentes. 
+ * Quelques exemples de formations avec leur nombre de stagiaires (nombre fictif) 
+ *  
+ *   WEB : 18
+ *   WAD : 15
+ *   Unity: 15
+ *   ASR : 20 
+ * 
+ *   Calculer et afficher le total de stagiaires pour ces formations 
  */
+
+
+
+ /**
+ * Exercice 2 
+ * 
+ *  Vous fêtez un anniversaire. 
+ *  Pour allumer les bougies du gâteau, vous utilisez 3 allumettes d'une boîte de 50. 
+ *  
+ *  Calculer et afficher le nombre qu'il reste. 
+ *  
+ *  Et si votre boîte d'allumettes en contenait 120 ? 
+ * 
+ */
+
+
+
+ /**
+ * Exercice 3
+ * 
+ *  Interface3 dispose de 10 salles de cours. 
+ *  Combien faudrait-il de stagiares par classe, s'il y 210 stagiaires inscrites?  
+ *  
+ *  Calculer et afficher ce nombre
+ * 
+ */
+
+ /**
+ * Exercice 4
+ * 
+ *  Vous êtes sorties entre  copines (5 au total), manger des pizzas, 12,56 EUR chacune. 
+ *  Combien sera l'addition ? 
+ *  Calculez et afficher ce nombre 
+ */
+
+
+ /**
+ * Exercice 5
+ * 
+ *  Calculer et afficher le reste de la division de 25 par 3
+ * 
+ */
+
+
+
+ 
 
 
     
