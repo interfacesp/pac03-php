374e5f96bab22c82b7c6cd0287b27227d026292b
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-04-04 22:37:30 +0200

    restructuration op. arith.

--- exemples-theorie/2-operations-arithmetiques.php
+++ exemples-theorie/2-operations-arithmetiques.php
@@ -1,57 +1,73 @@
 <?php
     
+ 
+
     /**
      * Addition 
      */
+    
+     echo "<h3>Addition</h3>";
 
      $nombre1 = 10; 
      $nombre2 = 11; 
      $resultatAdd = $nombre1 + $nombre2; 
 
-     echo " $nombre1 + $nombre2 : ". $resultatAdd; 
+     echo " $nombre1 + $nombre2 = ". $resultatAdd; 
 
      /**
      *  Soustraction 
      */
 
+    echo "<h3>Soustraction</h3>";
+
+
     $nombre1 = 10; 
     $nombre2 = 11; 
     $resultatMinus = $nombre1 - $nombre2; 
 
-    echo "$nombre1 - $nombre2 : ". $resultatMinus; 
+    echo "$nombre1 - $nombre2 = ". $resultatMinus; 
 
 
     /**
      *  Multiplication
      */
 
+    echo "<h3>Multiplaction</h3>";
+
+
     $nombre1 = 10; 
     $nombre2 = 11; 
     $resultatMulti = $nombre1 * $nombre2; 
 
-    echo "Résultat Multiplication: ". $resultatMulti; 
+    echo "$nombre1 * $nombre2 =  ". $resultatMulti; 
 
 
     /**
      *  Division 
      */
 
+    echo "<h3>Division</h3>";
+
+
     $nombre1 = 10; 
     $nombre2 = 2; 
     $resultatDiv = $nombre1 / $nombre2; 
 
-    echo "Résultat Div: ". $resultatDiv; 
+    echo "$nombre1 / $nombre2 = ". $resultatDiv; 
 
 
     /**
      *  Modulo : le reste de la division 
      */
 
+    echo "<h3>Modulo</h3>";
+
+
     $nombre1 = 10; 
     $nombre2 = 2; 
     $resultatMod = $nombre1 % $nombre2; 
 
-    echo "Résultat Modulo: ". $resultatMod; 
+    echo "$nombre1 % $nombre2 =  ". $resultatMod; 
      
 
 ?>
