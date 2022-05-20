d64a44ec14898fda46f7ba8ef9a75de55a0c5433
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-05-20 14:46:51 +0200

    exerices tableaux

---
+++ exercices/1_fondamentaux/4c-ex-tableaux-bonus.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableaux - Exercices Bonus</title>
+</head>
+<body>
+
+<h3>Exercies Tableaux - Bonus</h3>
+
+    <h4>Mes Passions</h4>
+
+    <?php
+        /**
+         * Afficher le contenu du tableau à l'aide d'une boucle for
+         */
+
+        $mesPassions = ["sport", "cuisine", "mots-croisés"];
+    ?>
+
+    <h4>Mes Passions 2</h4>
+
+    <?php
+        /**
+         * 1. Ajouter 3 nouvelles passions dans le tableau précédent (exemple: kayak,foot,peinture) 
+         * 2. Afficher le contenu avec une boucle for
+         */
+    ?>
+
+
+    <h4>Mes Passions 3</h4>
+
+    <?php
+
+        /**
+         * Afficher le premier et dernier élément du tableau des mes passions 
+         */
+    ?>
+
+    <h4>Mes Passions 4</h4>
+    
+    
+</body>
+</html>
