3fd46593ef9852f004458817b9afae945e5c11a7
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-06-13 11:46:33 +0200

    exercices tab assoc  et fonctions

---
+++ exercices/1_fondamentaux/4c-ex-tableaux-assoc-bonus.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercices - Bonus - Tableaux associatifs </title>
+</head>
+<body>
+
+<?php
+
+    /**
+     * Déclarer un tableau association: calendrier. 
+     * Les clés seront les noms des mois de l'années
+     * Les valeurs seront le nombre de jours dans cette année.
+     * Une clé spéciale "année" contiendra l'année
+     *  
+     */
+
+
+     /**
+      * Afficher le nombre de jours du mois d'octobre 
+      */
+
+
+      /**
+       * Afficher le nombre de jours du mois de février 
+       */
+
+
+      /**
+       * 
+       *  Afficher le contenu du calendrier (exemple "mars: 31") avec boucle foreach 
+       * (indice: regardez documentation PHP pour vous inspirer de comment parcourir )
+       * */ 
+
+       
+
+?>
+
+    
+</body>
+</html>
---
+++ exercices/2_dev-web/recapitulatif.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Récapitulatif </title>
+</head>
+<body>
+
+<h2>Exercices Récapitulatifs</h2>
+
+<?php
+
+/**
+ * Déclarer deux variablesle  : nom et prénom. 
+ */
+
+
+    /**
+     * Affiche le contenu de la variable prénom
+    */
+
+    
+    /**
+       * Afficher le contenu de la variable nom 
+    */
+
+    /**
+     * Afficher concaténation Nom et Prénom 
+     */
+
+    /**
+     * Déclarer une variable prix.
+     * Déclaration une variable réduction de 30% 
+     * Afficher le prix après application de la réduction 
+     */
+
+
+     /**
+      * Déclarer une variable avec un nombre au hasard. 
+      * Afficher "Nombre pair" si le nombre est pair
+      * ou "Nombre impair" si le nombre est impair
+      */
+
+    /**
+     * Déclarer un tableau avec les prénoms de toute la classe
+     */
+    
+
+    
+
+    /**
+     * Afficher le premier prénom de la liste
+    */
+
+    /**
+     * Afficher le dernier prénom de la liste
+     */
+
+
+    /**
+     * Afficher  la liste des prénoms avec une boucle for? 
+     */
+
+    /**
+     * Afficher  la liste des prénoms avec une boucle foreach? 
+    */
+    
+    /**
+     * Afficher la liste des prénoms avec boucle while 
+     */
+
+
+?>
+
+
+    
+</body>
+</html>
+
