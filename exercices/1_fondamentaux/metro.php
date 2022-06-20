5d2b9156f1b24e0beabda39a3a028b8793691658
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-06-20 09:56:31 +0200

    recap 20 juin

--- exercices/1_fondamentaux/metro.php
+++ exercices/1_fondamentaux/metro.php
@@ -8,6 +8,9 @@
                     "Lemmonier",
                     "Gare du Midi"  
     ];
+
+    $nombreArretsTram4 = count($arretsTram4); 
+
             
     $arretsMetro1 = [ "Gare De l'Ouest",
                       "Beekant",
@@ -21,8 +24,24 @@
                       "Schuman",
                       "Merode"
     ];
+    $nombreArretsMetro1 = count($arretsMetro1);
     
-    
-    
+    $nombreArretsTotals= $nombreArretsTram4 + $nombreArretsMetro1;
+
+    //Le nombre d'arrêts du réseau est 18
+    echo "Le nombre d'arrêts du réseau est ".$nombreArretsTotals."<br>";
+
+    //Il y a 18 arrêts sur tout le réseau.
+    echo "Il y a ".$nombreArretsTotals." arrêts sur tout le réseau <br>";
+    echo 'Il y a $nombreArretsTotals arrêts sur le réseau <br>'; //n'évalue pas la variable nombreArretsTotals
+    echo "Il y a $nombreArretsTotals arrêts sur le réseau <br>"; 
+
+    for($indice=0;$indice <count($arretsTram4);$indice++){
+        echo $arretsTram4[$indice]."<br/>";
+    }
+
+    for ($curseur = 0; $curseur < $nombreArretsMetro1; $curseur++){
+        echo $arretsMetro1[$curseur]."<br>";
+    }
 
 ?>
