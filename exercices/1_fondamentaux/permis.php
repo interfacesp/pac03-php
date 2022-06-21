021f73b1e2cc2ec7b041f71d8ff01b302de236e1
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-06-21 15:23:46 +0200

    exercice permis

---
+++ exercices/1_fondamentaux/permis.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Ecrivez un programme PHP de sorte 
+ * 2 variables booléennes: drivingLicense, car 
+ * 
+ * 'drivingLicense':  true ou false - détermine si on a le permis ou pas
+ * 'car' : true ou false - détermine si on a une voiture ou pas
+ * 
+ *  Si on a le permis 
+ *       
+ *             si on a une voiture 
+ *                       écrivez "conduisez à la gare"
+*               sinon 
+*                       écrivez "vous pouvez louer une voiture pas chère
+ *  sinon
+ *              si on a une voiture 
+ *                      écrivez "hors la loi"
+ *              
+ *              sinon
+ *                      écrivez "prenez un villo"
+ *             
+ *
+ *  
+ */ 
+
+
+ ?> 
