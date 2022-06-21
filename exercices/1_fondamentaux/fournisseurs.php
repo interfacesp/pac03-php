a81f16089ee3a7e200457a586f8b0e5293a60fc0
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-06-21 16:39:19 +0200

    correction fournisseurs

--- exercices/1_fondamentaux/fournisseurs.php
+++ exercices/1_fondamentaux/fournisseurs.php
@@ -12,25 +12,42 @@
      * Afficher l'adresse du fournisseur 
      */
 
+     echo $fournisseur['addresse']. "<br>"; 
 
      /**
      * Ajouter le pays : Belgique
      */
-
-
-     /**
-     * Afficher le nom 
-     */
+    $fournisseur['pays'] = "Belgique";
+    //ajout d'une cle sans valeur: $fournisseur['cle-sans-valeur'];
 
      /**
      * Afficher le nom 
      */
+
+     echo $fournisseur['nom'];
+
 
      /**
       * Parcourez les clé-valeurs, avec une boucle foreach
       */
 
+    echo "<h4>Parcours avec Foreach</h4>";
 
+    foreach($fournisseur as $cle => $valeur){
+        echo $cle."<br>". $valeur. "<br>";
+    }
+
+    echo "<h4>Autre format affichage </h4>"; 
+
+    foreach($fournisseur as $cle => $valeur){
+        echo $cle." : ". $valeur. "<br>";
+    }
+
+    echo "<h4>Troisième format affichage </h4>"; 
+
+    foreach($fournisseur as $cle => $valeur){
+        echo "<p> $cle : $valeur</p>";
+    }
 
      /**
       * Créer une tableau indexé, composé de 3 fournisseurs, dont vous choisirez les valeurs de votre choix. 
