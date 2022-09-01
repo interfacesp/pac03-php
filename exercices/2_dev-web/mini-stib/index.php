52f58633e2314290c1cae9b82a10ca50c3a01c27
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-09-01 14:15:49 +0200

    mini-stib

--- exercices/2_dev-web/mini-stib/index.php
+++ exercices/2_dev-web/mini-stib/index.php
@@ -25,34 +25,76 @@
 </head>
 <body>
 
+        <?php 
+
+            echo "<h4>Toutes les Lignes</h4>";
+            
+            $sqlQueryLines = "SELECT * FROM line";
+            $statement = $mysqlClient->prepare($sqlQueryLines); 
+            $statement->execute();
+
+            $result = $statement->fetchAll(); 
+
+
+            foreach($result as $aLine){
+
+                    echo "<p>";
+                    echo $aLine['id']; 
+                    echo $aLine['line_name']; 
+                    echo $aLine['distance']; 
+                    echo "</p>";
+                
+            }
+
+            echo "<h4>Nombre de Lignes</h4>";
+
+            /**
+             * Comptage du nombre de lignes 
+             */
+            $queryCount = "SELECT count(*) FROM line";
+            $statement = $mysqlClient->prepare($queryCount); 
+            $statement->execute();  
+
+            $resultCount = $statement->fetchAll(); 
+    
+            // echo "<pre>";
+            // print_r($resultCount);
+            // echo "</pre>";
+
+            echo "Nombre Lignes ". $resultCount[0]['count(*)'];
+            
+            
+            echo "<h4>Lignes et leurs arrêts</h4>";
+
+            /**
+             * Quelles sont les Lignes ainsi que leurs Arrêts
+             * 
+             */
+
+             $queryLinesAndStops= "
+                                SELECT * 
+                                FROM line,stop 
+                                WHERE line.id = stop.id_line
+                            ";
+
+            $statement = $mysqlClient->prepare($queryLinesAndStops);
+            $statement->execute();
+            $resultLinesStops = $statement->fetchAll();
+
+            $nbResults = count($resultLinesStops);
+            for($i = 0; $i < $nbResults; $i++){
+
+               echo  "<p> Ligne: ". $resultLinesStops[$i]['line_name']. "</p>";
+               echo  " <p>Distance Ligne: ". $resultLinesStops[$i]['distance']. "</p>";
+               echo "<p>Stop: ". $resultLinesStops[$i]['stop_name']."</p>";
+            }
+
+        ?>
+
+
     
 
-    <?php 
-        
-        $sqlQueryLines = "SELECT * FROM line";
-        $statement = $mysqlClient->prepare($sqlQueryLines); 
-        $statement->execute();
-
-        $result = $statement->fetchAll(); 
-
-
-
-        foreach($result as $aLine){
-
-            echo "<p>";
-            echo $aLine['id']; 
-            echo $aLine['line_name']; 
-            echo $aLine['distance']; 
-            echo "</p>";
-
-            
-        }
-
-
-        // echo '<pre>';
-        // print_r($result);
-        // echo '</pre>'; 
-    ?>
+    
 
 
 
