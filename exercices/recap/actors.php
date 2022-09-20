08c26ee02370f76cbec27523fd05746a2bd64060
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-09-20 16:12:20 +0200

    exemple table html

---
+++ exercices/recap/actors.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Actors</title>
+</head>
+
+<body>
+
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    <td>Id</td>
+                    <td>Description</td>
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+
+            <tr>
+
+                <!-- <td>
+                </td> -->
+                <td>
+                    A
+                </td>
+                <td>
+                    Tipi
+                </td>
+            </tr>
+
+            <tr>
+
+                <!-- <td>
+</td> -->
+                <td>
+                    Dr
+                </td>
+                <td>
+                    Yurt
+                </td>
+            </tr>
+
+
+            <tr>
+
+                <!-- <td>
+</td> -->
+                <td>
+                    3
+                </td>
+                <td>
+                    Tente Quechua
+                </td>
+            </tr>
+
+
+        </tbody>
+
+
+    </table>
+
+</body>
+
+</html>
